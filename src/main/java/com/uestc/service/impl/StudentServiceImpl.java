48da7c31162339edfe923cd907a79ebb215ec5b0
ka1i <GuzalInspire@hotmail.com>
2019-08-02 20:43:11 +0800

    change system layout

---
+++ src/main/java/com/uestc/service/impl/StudentServiceImpl.java
@@ -0,0 +1,63 @@
+package com.uestc.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.uestc.bean.LoginForm;
+import com.uestc.bean.Student;
+import com.uestc.dao.StudentMapper;
+import com.uestc.service.StudentService;
+
+import java.util.List;
+
+/**
+ * @project: sms
+ * @description: 业务层实现类-操控学生信息
+ * @author: 黄宇辉
+ * @date: 6/16/2019-10:51 AM
+ * @version: 1.0
+ * @website: https://yubuntu0109.github.io/
+ */
+@Service
+@Transactional
+public class StudentServiceImpl implements StudentService {
+
+    //注入Mapper接口对象
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @Override
+    public Student login(LoginForm loginForm) {
+        return studentMapper.login(loginForm);
+    }
+
+    @Override
+    public Student fingBySno(Student student) {
+        return studentMapper.findBySno(student);
+    }
+
+    @Override
+    public List<Student> selectList(Student student) {
+        return studentMapper.selectList(student);
+    }
+
+    @Override
+    public int insert(Student student) {
+        return studentMapper.insert(student);
+    }
+
+    @Override
+    public int update(Student student) {
+        return studentMapper.update(student);
+    }
+
+    @Override
+    public int updatePassowrd(Student student) {
+        return studentMapper.updatePassword(student);
+    }
+
+    @Override
+    public int deleteById(Integer[] ids) {
+        return studentMapper.deleteById(ids);
+    }
+}
